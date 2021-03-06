411f8f6f544a8e05a659753664dfa2213d832bf2
Fraser McCallum <fraser@themccallums.net.nz>
2021-03-06 22:52:48 +1300

    🎉 Update test commands with new decorators

--- src/RealCommand/TestCommand.ts
+++ src/RealCommand/TestCommand.ts
@@ -1,19 +1,28 @@
-import Argument from "../annotations/Argument";
+import { Message } from "discord.js";
+import Argument from "../commands/arguments/Argument";
+import Alias from "../commands/alias/Alias";
 import Command from "../commands/Command";
+import Inhibit from "../commands/inhibitor/Inhibit";
 import BooleanType from "../types/boolean";
+import FloatType from "../types/float";
 import IntegerType from "../types/integer";
+import UnionType from "../types/union";
+import Permit from "../commands/permissions/Permit";
 
+@Inhibit({ limitBy: "USER", maxUsesPerPeriod: 3, periodDuration: 10 })
+@Alias("hello", "test")
+@Permit("ADMINISTRATOR")
 export default class TestCommand extends Command {
     @Argument({ type: new BooleanType() })
     someBoolean!: boolean;
+
     @Argument({ type: new IntegerType(), validate: (n) => n < 50 })
+    someInteger!: number;
+
+    @Argument({ type: new UnionType(new IntegerType(), new FloatType()) })
     someNumber!: number;
 
-    commandShouldInvoke(command: string) {
-        return ["test", "experiment", "testing"].includes(command);
-    }
-
-    execute() {
-        console.log(`Executed! Args: ${this.number} ${this.notArg}`);
+    execute(message: Message) {
+        console.log(`Executed! Args: ${this.someBoolean} ${this.someInteger} ${this.someNumber} `);
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,28 @@
 import path from "path";
-import Argument, { getArguments } from "./annotations/Argument";
+import Argument, { getArguments } from "./commands/arguments/Argument";
 import Client from "./Client";
 import Command from "./commands/Command";
 import BooleanType from "./types/boolean";
+import { BitField } from "discord.js";
 
 (async () => {
     const client = new Client({ defaultCommandPrefix: "%", owners: [] });
     await client.registry.recursivelyRegisterCommands(path.join(__dirname, "./RealCommand"));
 
     //@ts-ignore
-    client.emit("message", { content: "hello yes  45" });
+    client.emit("message", {
+        content: "test yes 45 12",
+        author: { id: "12345" },
+        member: { permissions: new BitField(0) },
+    });
+    //@ts-ignore
+    client.emit("message", { content: "hello yes 45 12", author: { id: "12345" } });
+    //@ts-ignore
+    client.emit("message", { content: "test yes 45 12", author: { id: "12345" } });
+    //@ts-ignore
+    client.emit("message", { content: "test yes 45 12", author: { id: "12345" } });
+    //@ts-ignore
+    client.emit("message", { content: "test yes 45 12", author: { id: "123455" } });
 
     // const cmd = new TestCommand(); console.log(getArguments(cmd));
     // console.log(cmd);
