ae27a972ba7ef74c303ac8181671dcf7568f6afc
Fraser McCallum <fraser@themccallums.net.nz>
2021-03-07 17:59:57 +1300

    🚚 Move client to different directory

---
+++ src/client/Client.ts
@@ -0,0 +1,73 @@
+import Log from "@frasermcc/log";
+import { Client as BaseClient, ClientOptions, Guild } from "discord.js";
+import { CommandRegistry } from "../commands/CommandRegistry";
+import Dispatcher from "../commands/Dispatcher";
+import GuildSettingsManager from "./GuildSettings";
+
+interface Options extends ClientOptions {
+    defaultCommandPrefix: string;
+    owners: string[];
+}
+
+export default class Client extends BaseClient {
+    private readonly _registry;
+    private readonly _dispatcher;
+
+    private readonly _guildSettings;
+
+    constructor(options: Options) {
+        super(options);
+
+        this._registry = new CommandRegistry(this);
+        this._dispatcher = new Dispatcher(this, this.registry);
+        this._guildSettings = new GuildSettingsManager(options.defaultCommandPrefix ?? "!");
+
+        this.on("message", (m) => this.dispatcher.handleRawMessage(m));
+    }
+
+    get guildSettingsManager() {
+        return this._guildSettings;
+    }
+
+    get registry() {
+        return this._registry;
+    }
+
+    get dispatcher() {
+        return this._dispatcher;
+    }
+
+    /**
+     * @deprecated
+     */
+    get prefix() {
+        Log.error(
+            "Accessing prefixes inside client is deprecated. Please access it from guildSettingsManager inside client."
+        );
+        return this.guildSettingsManager.prefix;
+    }
+
+    /**
+     * @deprecated
+     * @param guild
+     */
+    getPrefixForGuild(guild: Guild | null): string {
+        Log.error(
+            "Accessing guild prefixes inside client is deprecated. Please access it from guildSettingsManager inside client."
+        );
+        if (!guild) return this.prefix;
+        return this.guildSettingsManager.getPrefixForGuild(guild);
+    }
+
+    /**
+     * @deprecated
+     * @param guild
+     * @param prefix
+     */
+    setPrefixForGuild(guild: Guild, prefix: string): void {
+        Log.error(
+            "Setting guild prefixes inside client is deprecated. Please access it from guildSettingsManager inside client."
+        );
+        this.guildSettingsManager.setPrefixForGuild(guild, prefix);
+    }
+}
