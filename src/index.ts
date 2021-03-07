1513ed6e9bb81679f8f9ecf8ede32625fd9e0726
Fraser McCallum <fraser@themccallums.net.nz>
2021-03-07 18:03:05 +1300

    🚚 Update import/export paths

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { BitField } from "discord.js";
 import path from "path";
-import Client from "./Client";
+import Client from "./client/Client";
 import Command from "./commands/Command";
 import Permit from "./commands/permissions/Permit";
 import Inhibit from "./commands/inhibitor/Inhibit";
@@ -26,22 +26,21 @@ export { Client, Command, Permit, Inhibit, Alias, Argument };
 //     const client = new Client({ defaultCommandPrefix: "%", owners: [] });
 //     await client.registry.recursivelyRegisterCommands(path.join(__dirname, "./RealCommand"));
 //     await client.registry.recursivelyRegisterEvents(path.join(__dirname, "./RealCommand"));
+//     client.guildSettingsManager.disableGroupInGuild({ guild: "guild123", groupName: "base", shouldBeDisabled: true });
 
 //     //@ts-ignore
 //     client.emit("message", {
 //         content: "%test yes 45 12",
 //         author: { id: "12345" },
 //         member: { permissions: new BitField(8) },
+//         guild: { id: "guild123" },
 //     });
-//     //@ts-ignore
-//     client.emit("message", { content: "%hello yes 45 12", author: { id: "12345" } });
-//     //@ts-ignore
-//     client.emit("message", { content: "%test yes 45 12", author: { id: "12345" } });
-//     //@ts-ignore
-//     client.emit("message", { content: "%test yes 45 12", author: { id: "12345" } });
-//     //@ts-ignore
-//     client.emit("message", { content: "%test yes 45 12", author: { id: "123455" } });
-
-//     const cmd = new TestCommand(); console.log(getArguments(cmd));
-//     console.log(cmd);
+//     // //@ts-ignore
+//     // client.emit("message", { content: "%hello yes 45 12", author: { id: "12345" } });
+//     // //@ts-ignore
+//     // client.emit("message", { content: "%test yes 45 12", author: { id: "12345" } });
+//     // //@ts-ignore
+//     // client.emit("message", { content: "%test yes 45 12", author: { id: "12345" } });
+//     // //@ts-ignore
+//     // client.emit("message", { content: "%test yes 45 12", author: { id: "123455" } });
 // })();
