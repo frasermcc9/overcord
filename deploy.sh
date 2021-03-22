0b089e827429f403233c4a21b846632492c2eaf7
Fraser McCallum <fraser@themccallums.net.nz>
2021-03-22 22:41:10 +1300

    Fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -9,17 +9,11 @@ npm run docs:build
 # navigate into the build output directory
 cd docs/src/.vuepress/dist
 
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
-
 git init
 git add -A
 git commit -m 'deploy'
 
-# if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
-
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:frasermcc9/overcord.git master:gh-pages
+git remote add origin https://github.com/frasermcc9/overcord
+git push -f -u origin master:gh-pages
 
 cd -
