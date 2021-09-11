45bf2467db809d53eef3aa9ab3ab2142df12a68d
Fraser McCallum <fraser@themccallums.net.nz>
2021-09-11 16:01:19 +1200

    Ignore bot messages in command handler

--- src/commands/Dispatcher.ts
+++ src/commands/Dispatcher.ts
@@ -3,25 +3,25 @@ import Client from "../client/Client";
 import { CommandRegistry } from "./CommandRegistry";
 
 export default class Dispatcher {
-    private client: Client;
-    private registry: CommandRegistry;
+  private client: Client;
+  private registry: CommandRegistry;
 
-    constructor(client: Client, registry: CommandRegistry) {
-        this.client = client;
-        this.registry = registry;
-    }
+  constructor(client: Client, registry: CommandRegistry) {
+    this.client = client;
+    this.registry = registry;
+  }
 
-    handleRawMessage(message: Message) {
-        const content = message.content.trim();
-        const prefix = this.client.guildSettingsManager.getPrefixForGuild(message.guild);
-        if (!content.startsWith(prefix)) {
-            return;
-        }
-        const cleanContent = content.replace(prefix, "");
-        const cleanedSpace = cleanContent.replace(/\s+/g, " ");
-        const fragments = Array.from(cleanedSpace.matchAll(/"[^"]+"|[^\s]+/g)).map((s) =>
-            s.toString().replace(/"/g, "")
-        );
-        this.registry.executeCommand({ fragments, message });
+  handleRawMessage(message: Message) {
+    if (message.author.bot) return;
+
+    const content = message.content.trim();
+    const prefix = this.client.guildSettingsManager.getPrefixForGuild(message.guild);
+    if (!content.startsWith(prefix)) {
+      return;
     }
+    const cleanContent = content.replace(prefix, "");
+    const cleanedSpace = cleanContent.replace(/\s+/g, " ");
+    const fragments = Array.from(cleanedSpace.matchAll(/"[^"]+"|[^\s]+/g)).map((s) => s.toString().replace(/"/g, ""));
+    this.registry.executeCommand({ fragments, message });
+  }
 }
